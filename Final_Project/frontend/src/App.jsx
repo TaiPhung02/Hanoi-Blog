8f81c58f19e7d070ce33ee4609d5887ae13f790e
TaiPhung02 <2051063445@e.tlu.edu.vn>
2024-04-11 22:15:15 +0700

    setting up routes

--- Final_Project/frontend/src/App.jsx
+++ Final_Project/frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <Navbar/>
-    )
-}
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="sign-in" element={<h1>Sign in</h1>} />
+                <Route path="sing-up" element={<h1>Sign up</h1>} />
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
