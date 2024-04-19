1b3a44d26b0168143191a0db112a002901422e44
TaiPhung02 <2051063445@e.tlu.edu.vn>
2024-04-19 23:30:19 +0700

    create blogeditor + navbar + banner

--- Final_Project/frontend/src/components/blog-editor.component.jsx
+++ Final_Project/frontend/src/components/blog-editor.component.jsx
@@ -1,5 +1,56 @@
+import { Link } from "react-router-dom";
+import AnimationWrapper from "../common/page-animation";
+import logo from "../imgs/logo.png";
+import defaultBanner from "../imgs/blog_banner.png";
+
 const BlogEditor = () => {
-    return <h1>Blog Editor from component</h1>;
+    const handleBannerUpload = (e) => {
+        let img = e.target.files[0];
+
+        console.log(img);
+    };
+
+    return (
+        <>
+            <nav className="navbar">
+                <Link to="/" className="flex-none w-10">
+                    <img src={logo} alt="logo" />
+                </Link>
+
+                <p className="max-md:hidden text-black line-clamp-1 w-full">
+                    New Blog
+                </p>
+
+                <div className="flex gap-4 ml-auto">
+                    <button className="btn-dark py-2">Publish</button>
+                    <button className="btn-light py-2">Save Draft</button>
+                </div>
+            </nav>
+
+            <AnimationWrapper>
+                <section>
+                    <div className="mw-auto max-w-[900px] w-full">
+                        <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+                            <label htmlFor="uploadBanner">
+                                <img
+                                    src={defaultBanner}
+                                    alt="defaultBanner"
+                                    className="z-20"
+                                />
+                                <input
+                                    id="uploadBanner"
+                                    type="file"
+                                    accept=".png, .jpg,. jpeg"
+                                    hidden
+                                    onChange={handleBannerUpload}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                </section>
+            </AnimationWrapper>
+        </>
+    );
 };
 
 export default BlogEditor;
