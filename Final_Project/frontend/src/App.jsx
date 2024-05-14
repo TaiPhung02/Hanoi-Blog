f127fe6a3add2555307feb8e019e29770e2ed47a
longnd8 <long.ngoduc@powergateaustralia.com>
2024-05-14 22:31:09 +0700

    setting up home route, home page, "-inPageNavigation"

--- Final_Project/frontend/src/App.jsx
+++ Final_Project/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext, useEffect, useState } from "react";
 import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
+import HomePage from "./pages/home.page";
 
 export const UserContext = createContext({});
 
@@ -23,6 +24,7 @@ const App = () => {
             <Routes>
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/" element={<Navbar />}>
+                    <Route index element={<HomePage/>} />
                     <Route
                         path="sign-in"
                         element={<UserAuthForm type="sign-in" />}
