3672e86989e2e4e2563b445b883b83b79b07de4b
longnd8 <long.ngoduc@powergateaustralia.com>
2024-05-20 21:13:53 +0700

    Working Search Input

--- Final_Project/frontend/src/components/navbar.component.jsx
+++ Final_Project/frontend/src/components/navbar.component.jsx
@@ -1,13 +1,14 @@
 import { useContext, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import { UserContext } from "../App";
 import UserNavigationPanel from "./user-navigation.component";
 
 const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
-
     const [userNavPanel, setUserNavPanel] = useState(false);
+
+    let navigate = useNavigate();
 
     const {
         userAuth,
@@ -17,6 +18,14 @@ const Navbar = () => {
     const handleUserNavPanel = () => {
         setUserNavPanel((currentVal) => !currentVal);
     };
+
+    const handleSearch = (e) => {
+      let query = e.target.value;
+
+      if (e.keyCode == 13 && query.length) {
+        navigate(`/search/${query}`);
+      }
+    }
 
     const handleBlur = () => {
         setTimeout(() => {
@@ -41,6 +50,7 @@ const Navbar = () => {
                         type="text"
                         placeholder="Search"
                         className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+                        onKeyDown={handleSearch}
                     />
 
                     <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
