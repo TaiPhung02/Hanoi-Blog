b1f5e44e40337bb275ffa1edca3f2e6ba680d060
longnd8 <long.ngoduc@powergateaustralia.com>
2024-06-15 14:55:50 +0700

    fix: User Profile Page

--- Final_Project/frontend/src/pages/profile.page.jsx
+++ Final_Project/frontend/src/pages/profile.page.jsx
@@ -36,6 +36,13 @@ const ProfilePage = () => {
   let [profileLoaded, setProfileLoaded] = useState("");
 
   let {
+    personal_info: { fullname, username: profile_username, profile_img, bio },
+    account_info: { total_posts, total_reads },
+    social_links,
+    joinedAt,
+  } = profile;
+
+  let {
     userAuth: { username },
   } = useContext(UserContext);
 
@@ -44,7 +51,7 @@ const ProfilePage = () => {
       .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-profile", {
         username: profileId,
       })
-      .then(({ data: { user } }) => {
+      .then(({ data: user }) => {
         if (user != null) {
           setProfile(user);
         }
@@ -106,21 +113,21 @@ const ProfilePage = () => {
         <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12">
           <div className="flex flex-col max-md:items-center gap-5 min-w-[250px] md:w-[50%] md:pl-8 md:border-l border-grey md:sticky md:top-[100px] md:py-10">
             <img
-              src={profile?.personal_info?.profile_img}
+              src={profile_img}
               alt="profile_img"
               className="w-48 h-48 bg-grey rounded-full md:w-32 md:h-32"
             />
 
             <h1 className="text-2xl font-medium">
-              @{profile?.personal_info?.username}
+              @{profile_username}
             </h1>
             <p className="text-xl capitalize h-6">
-              {profile?.personal_info?.fullname}
+              {fullname}
             </p>
 
             <p>
-              {profile?.account_info?.total_posts?.toLocaleString()} Blogs -{" "}
-              {profile?.account_info?.total_reads?.toLocaleString()} Reads
+              {total_posts?.toLocaleString()} Blogs -{" "}
+              {total_reads?.toLocaleString()} Reads
             </p>
 
             <div className="flex gap-4 mt-2">
@@ -138,9 +145,9 @@ const ProfilePage = () => {
 
             <AboutUser
               className="max-md:hidden"
-              bio={profile?.personal_info?.bio}
-              social_links={profile?.social_links}
-              joinedAt={profile?.joinedAt}
+              bio={bio}
+              social_links={social_links}
+              joinedAt={joinedAt}
             />
           </div>
 
@@ -173,9 +180,9 @@ const ProfilePage = () => {
               </>
 
               <AboutUser
-                bio={profile?.personal_info?.bio}
-                social_links={profile?.social_links}
-                joinedAt={profile?.joinedAt}
+                bio={bio}
+                social_links={social_links}
+                joinedAt={joinedAt}
               />
             </InPageNavigation>
           </div>
