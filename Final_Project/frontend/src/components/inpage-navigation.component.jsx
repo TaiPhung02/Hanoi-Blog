596aab06c31b39e04b8645f7afe637c117965251
TaiPhung02 <2051063445@e.tlu.edu.vn>
2024-06-29 15:42:37 +0700

    fix: InPageNavigation UI

--- Final_Project/frontend/src/components/inpage-navigation.component.jsx
+++ Final_Project/frontend/src/components/inpage-navigation.component.jsx
@@ -3,12 +3,20 @@ import { useEffect, useRef, useState } from "react";
 export let activeTabLineRef;
 export let activeTabRef;
 
-const InPageNavigation = ({ routes, defaultHidden = [], defaultActiveIndex = 0, children }) => {
-
+const InPageNavigation = ({
+  routes,
+  defaultHidden = [],
+  defaultActiveIndex = 0,
+  children,
+}) => {
   activeTabLineRef = useRef();
   activeTabRef = useRef();
 
   let [inPageNavIndex, setInPageNavIndex] = useState(defaultActiveIndex);
+
+  let [isResizeEventAdded, setIsResizeEventAdded] = useState(false);
+
+  let [width, setWidth] = useState(window.innerWidth);
 
   const changePageState = (btn, i) => {
     let { offsetWidth, offsetLeft } = btn;
@@ -17,37 +25,52 @@ const InPageNavigation = ({ routes, defaultHidden = [], defaultActiveIndex = 0, 
     activeTabLineRef.current.style.left = offsetLeft + "px";
 
     setInPageNavIndex(i);
-  }
+  };
 
   useEffect(() => {
-    changePageState(activeTabRef.current, defaultActiveIndex)
-  }, [])
+    if (width > 766 && inPageNavIndex != defaultActiveIndex) {
+      changePageState(activeTabRef.current, defaultActiveIndex);
+    }
+
+    if (!isResizeEventAdded) {
+      window.addEventListener("resize", () => {
+        if (!isResizeEventAdded) {
+          setIsResizeEventAdded(true);
+        }
+
+        setWidth(window.innerWidth);
+      });
+    }
+  }, [width]);
 
   return (
     <>
       <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
-        {
-          routes.map((route, i) => {
-            return (
-              <button
-                ref={i == defaultActiveIndex ? activeTabRef : null}
-                key={i}
-                className={"p-4 px-5 capitalize " + ( inPageNavIndex == i ? "text-black" : "text-dark-grey " ) + ( defaultHidden.includes(route) ? " md:hidden " : " ")}
-                onClick={(e) => {changePageState(e.target, i)}}>
-                { route }
-              </button>
-            );
-          })
-        }
+        {routes.map((route, i) => {
+          return (
+            <button
+              ref={i == defaultActiveIndex ? activeTabRef : null}
+              key={i}
+              className={
+                "p-4 px-5 capitalize " +
+                (inPageNavIndex == i ? "text-black" : "text-dark-grey ") +
+                (defaultHidden.includes(route) ? " md:hidden " : " ")
+              }
+              onClick={(e) => {
+                changePageState(e.target, i);
+              }}
+            >
+              {route}
+            </button>
+          );
+        })}
 
-        <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300"/>
-
+        <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300" />
       </div>
 
-      { Array.isArray(children) ? children[inPageNavIndex] : children }
-
+      {Array.isArray(children) ? children[inPageNavIndex] : children}
     </>
-  )
-}
+  );
+};
 
 export default InPageNavigation;
